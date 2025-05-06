c10d31cf2a5e5b13f40681a3666213e6169aada5
WindowsOnAMac <0winonamac0@gmail.com>
2025-05-05 21:58:10 -0400

    Update TitleMenu

--- mods/Bopimo! Mod Manager/Scripts/Singleplayer/TitleMenu.gd
+++ mods/Bopimo! Mod Manager/Scripts/Singleplayer/TitleMenu.gd
@@ -7,9 +7,13 @@ func _ready() -> void:
 	level.load_level_from_file(bop_file_path)
 	
 	# Sets labels of version information for the client as well as godot
-	$UI/Version.text =  "%s | Modloader %s" % [Bopimo.GAME_VERSION, ProjectSettings.get_setting_with_override("modloader/version_string")]
+	$UI/Version.text = Bopimo.GAME_VERSION + " | Modloader " + ProjectSettings.get_setting_with_override("modloader/version_string")
 	$UI/Godot.text = Bopimo.godot_info
 	$UI/BuildInfo.text = Debug.get_builder_information_string()
+
+func _notification(what: int)->void:
+	if what == NOTIFICATION_TRANSLATION_CHANGED:
+		$UI/BuildInfo.text = Debug.get_builder_information_string()
 
 func _process(delta: float) -> void:
 	$CamPivot.rotate_y(delta * 0.2)
@@ -29,8 +33,13 @@ func _on_play_singleplayer_pressed() -> void:
 func _on_exit_pressed() -> void:
 	get_tree().quit()
 
+var settings_menu: ClientSettingsMenu
 func _on_settings_pressed() -> void:
-	$UI/SettingsInterface.show()
+	if not is_instance_valid(settings_menu):
+		settings_menu = ClientSettingsMenu.new()
+		settings_menu.theme = load("uid://cqkapog3tcgal")
+		settings_menu.add_child(ClientSettingsMenuBinder.new())
+		add_child(settings_menu)
 
 func _on_tutorial_pressed() -> void:
 	get_tree().change_scene_to_file("res://Scenes/Singleplayer/Tutorial.tscn")
@@ -43,7 +52,5 @@ func _on_close_pressed() -> void:
 func _on_server_button_pressed() -> void:
 	$UI/JoinServerMenu.show()
 
-
 func _on_mods_pressed() -> void:
-	#print("a")
 	$UI/ModsInterface.show()
